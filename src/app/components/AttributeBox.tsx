7b1ea6438b25b1e104242aced8c7959622ac9941
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2024-12-23 17:50:11 +0000

    Make a checkboxTracker component and used that in attribute box as well since it'll be reusable in the skillBox.

--- src/app/components/AttributeBox.tsx
+++ src/app/components/AttributeBox.tsx
@@ -1,7 +1,7 @@
-import { ChangeEvent, ReactNode, useEffect, useState } from "react";
 import './../styles/AttributeBox.css'
 import classNames from "classnames";
 import { ComponentProps } from "./Component";
+import CheckboxTracker from './CheckboxTracker';
 
 
 type Props = ComponentProps & {
@@ -12,67 +12,13 @@ type Props = ComponentProps & {
 }
 
 export default function AttributeBox (props: Props) {
-    const {label} = props;
-    const [currentValue, setCurrentValue] = useState(0);
-
-    useEffect(() => {
-        setCurrentValue(props.currentValue);
-    }, [setCurrentValue, props.currentValue])
-
-
-    const onChange = (event: ChangeEvent<HTMLInputElement>) =>{
-        const checked = event.target.checked;
-        let newValue = currentValue;
-
-        if(checked){
-            newValue += 1;
-
-            if(newValue > props.maxValue){
-                newValue = props.maxValue;
-            }
-        }
-        else{
-            newValue -= 1;
-
-            if(newValue < 0){
-                newValue = 0;
-            }
-        }
-
-        if(newValue != currentValue){
-            props.onValueChange?.(newValue);
-        }
-
-        setCurrentValue(newValue);        
-    }
-
-
-    function createCheckboxButtons(){
-        const checkboxButtons: ReactNode[] = [];
-
-        for (let index = 0; index < props.maxValue; index++) {
-            const checkboxButton = 
-            <input 
-                className="roundedCheckbox"
-                type="checkbox" 
-                checked={index < currentValue}
-                onChange={onChange}
-                key={`AttributeBox_${label}_${index}`}
-            />;
-
-            checkboxButtons.push(checkboxButton);
-        }
-
-        return checkboxButtons;
-    }
 
     return <div className={classNames('AttributeBox_root', props.baseClassName, props.classModifiers)}>
-        <div className="AttributeBox_label">
-            {label}
-        </div>
-        <div className="AttributeBox_checkboxButtonContainer">
-            {createCheckboxButtons()}
-        </div>
+        <CheckboxTracker
+            label={props.label} 
+            maxValue={props.maxValue} 
+            currentValue={props.currentValue} 
+            onValueChange={props.onValueChange}
+        />
     </div>
-   
 }
---
+++ src/app/components/CheckboxTracker.tsx
@@ -0,0 +1,76 @@
+import { ChangeEvent, ReactNode, useEffect, useState } from "react";
+import { ComponentProps } from "./Component"
+import './../styles/CheckboxTracker.css'
+import classNames from "classnames";
+
+type Props = ComponentProps & {
+    label: string,
+    maxValue: number,
+    currentValue: number,
+    onValueChange?: (value: number) => void,
+}
+
+export default function CheckboxTracker (props: Props){
+
+    const {label} = props;
+    const [currentValue, setCurrentValue] = useState(0);
+
+    useEffect(() => {
+        setCurrentValue(props.currentValue);
+    }, [setCurrentValue, props.currentValue])
+
+
+    function createCheckboxButtons(){
+        const checkboxButtons: ReactNode[] = [];
+
+        for (let index = 0; index < props.maxValue; index++) {
+            const checkboxButton = 
+            <input 
+                className={classNames("roundedCheckbox", props.classModifiers)}
+                type="checkbox" 
+                checked={index < currentValue}
+                onChange={onChange}
+                key={`AttributeBox_${label}_${index}`}
+            />;
+
+            checkboxButtons.push(checkboxButton);
+        }
+
+        return checkboxButtons;
+    }
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) =>{
+        const checked = event.target.checked;
+        let newValue = currentValue;
+
+        if(checked){
+            newValue += 1;
+
+            if(newValue > props.maxValue){
+                newValue = props.maxValue;
+            }
+        }
+        else{
+            newValue -= 1;
+
+            if(newValue < 0){
+                newValue = 0;
+            }
+        }
+
+        if(newValue != currentValue){
+            props.onValueChange?.(newValue);
+        }
+
+        setCurrentValue(newValue);        
+    }
+
+    return <div className={classNames('CheckboxTracker_root', props.baseClassName, props.classModifiers)}>
+        <div className={classNames("CheckboxTracker_label", props.classModifiers)}>
+            {props.label}
+        </div>
+        <div className={classNames("CheckboxTracker_checkboxButtonContainer", props.classModifiers)}>
+            {createCheckboxButtons()}
+        </div>
+    </div>
+}
